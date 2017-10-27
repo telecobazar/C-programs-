37ab0f92609ca0d05f13b1e2db1a4ea7db78980f
telecobazar <telecobazar@gmail.com>
2017-10-27 14:38:17 +0200

    primer programa en C

---
+++ 2017/00_primitivas/nombre.cpp
@@ -0,0 +1,10 @@
+#include <stdlib.h> /*directivas de cabecera*/
+#include <stdio.h>
+
+int main (){
+
+	printf("Hello! My name is Eric Martín\n"); /*llamada a la funcion*/
+
+	return EXIT_SUCCESS; 
+
+}
---
+++ 2017/skel.cpp
@@ -0,0 +1,9 @@
+#include <stdlib.h> % directivas de cabecera
+#include <stdio.h>
+
+int main (){
+
+
+	return EXIT_SUCCESS; 
+
+}
